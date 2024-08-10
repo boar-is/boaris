c3203dd69411c394311faab036771d593b187523
Boris Zubchenko <boriszubc@gmail.com>
2024-08-10 22:43:03 +0300

    backup

--- components/providers/server.providers.tsx
+++ components/providers/server.providers.tsx
@@ -1,7 +1,18 @@
 import { Toaster } from 'sonner'
+import { cs } from '~/lib/cs'
 
 export function ToastProvider() {
-  return <Toaster theme="dark" />
+  return (
+    <Toaster
+      theme="dark"
+      toastOptions={{
+        classNames: {
+          toast: cs('bg-gray-2 text-gray-11 border-gray-6 font-sans'),
+          title: cs('font-semibold'),
+        },
+      }}
+    />
+  )
 }
 
 export { LocalizedStringProvider } from 'react-aria-components/i18n'
