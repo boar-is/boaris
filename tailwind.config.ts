f3fe4bda8187def22d01b79754156a78bb5b6643
Boris Zubchenko <boriszubc@gmail.com>
2024-08-10 19:12:23 +0300

    backup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,16 @@
 import type { Config } from 'tailwindcss'
+import tailwindAnimate from 'tailwindcss-animate'
+import tailwindRac from 'tailwindcss-react-aria-components'
 
-const config: Config = {
+const tailwindConfig = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
-  plugins: [],
-}
-export default config
+  plugins: [tailwindAnimate, tailwindRac],
+} satisfies Config
+
+export default tailwindConfig
