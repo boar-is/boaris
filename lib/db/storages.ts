2f49bf3a6fbd28ffc578d2ad83b7711b366af27a
Boris Zubchenko <boriszubc@gmail.com>
2024-08-19 00:30:28 +0300

    backup

--- lib/db/storages.ts
+++ lib/db/storages.ts
@@ -21,27 +21,3 @@ export const storageDocs: ReadonlyArray<StorageDoc> = [
     _creationTime: Date.now(),
   },
 ]
-
-export class StorageRepository {
-  static #data: ReadonlyArray<StorageDoc> = [
-    {
-      _id: '1',
-      src: 'https://avatars.githubusercontent.com/u/31354262?v=4',
-      _creationTime: Date.now(),
-    },
-    {
-      _id: '2',
-      src: '/logo.png',
-      _creationTime: Date.now(),
-    },
-    {
-      _id: '3',
-      src: 'https://picsum.photos/seed/promises/1024/768',
-      _creationTime: Date.now(),
-    },
-  ]
-
-  static findOneSrc(id: StorageDoc['_id']) {
-    return StorageRepository.#data.find((it) => it._id === id)?.src
-  }
-}
