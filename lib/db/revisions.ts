05d09997d8ee9f720bc3df2a3ce6b34e1bb9e151
Boris Zubchenko <boriszubc@gmail.com>
2024-08-19 23:55:38 +0300

    backup

--- lib/db/revisions.ts
+++ lib/db/revisions.ts
@@ -1,14 +1,37 @@
-import type { Doc } from '~/lib/db/_shared'
-import type { PostDoc } from '~/lib/db/posts'
+import type { Doc } from './_shared'
+import type { PostDoc } from './posts'
+
+type Interpolation<T = number> = Array<[input: number, output: T]>
 
 export type RevisionDoc = Doc & {
   postId: PostDoc['_id']
+  captions?:
+    | {
+        /**
+         * JSON.stringified JSONContent
+         */
+        content: string
+        interpolation: Interpolation
+      }
+    | undefined
+  /**
+   * Breakpoints, like framer, a single unique track
+   */
+  layouts: unknown
+  files: Array<{
+    id: string
+    path: string
+    /**
+     * `null` means not created yet or deleted
+     */
+    initialValue: string
+    /**
+     * `null` output's for void, i.e., nothing happens
+     */
+    interpolation: Interpolation<{
+      interpolation: Interpolation
+    } | null>
+  }>
 }
 
-export const revisionDocs: ReadonlyArray<RevisionDoc> = [
-  {
-    _id: '1',
-    _creationTime: Date.now(),
-    postId: '1',
-  },
-]
+export const revisionDocs: Array<RevisionDoc> = []
