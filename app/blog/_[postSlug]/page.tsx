dd2958b74f180054d9a9f07500dd6a51abc8523f
Boris Zubchenko <boriszubc@gmail.com>
2024-09-11 17:03:52 +0300

    backup

--- app/blog/_[postSlug]/page.tsx
+++ app/blog/_[postSlug]/page.tsx
@@ -1,5 +1,7 @@
 import { notFound } from 'next/navigation'
+import { getBlogPost } from '~/lib/api/get-blog-post'
 import { postDocs } from '~/lib/model/docs/posts'
+import { BlogPostPageClient } from './page.client'
 
 export async function generateStaticParams() {
   return postDocs
@@ -12,20 +14,11 @@ export async function generateStaticParams() {
 export default async function BlogPostPage({
   params: { postSlug },
 }: { params: { postSlug: string } }) {
-  const post = postDocs[0]
+  const post = await getBlogPost(postSlug)
 
   if (!post) {
     notFound()
   }
 
-  return (
-    <main>
-      <video autoPlay muted loop playsInline width="1000">
-        <source
-          src="/deferred-example/shadow-palette-initial.mp4"
-          type="video/mp4"
-        />
-      </video>
-    </main>
-  )
+  return <BlogPostPageClient post={post} />
 }
