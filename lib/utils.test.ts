762dbf754108c4466648f4aede020985398883d8
Boris Zubchenko <boriszubc@gmail.com>
2024-09-13 21:57:42 +0300

    backup

--- lib/utils.test.ts
+++ lib/utils.test.ts
@@ -6,13 +6,13 @@ describe('mapSkippedPair', () => {
   const digits = 5
   const toFixed = toFixedNumber()(digits)
 
-  it('should interpolate 1', () => {
+  it('should interpolate the example 1', () => {
     expect(
       mapSkippedPair([0, 0.25, 0.75], [true, false, true], digits),
     ).toEqual([[0, 0.5, 0.5, 1].map(toFixed), [0, 0.25, 0.75, 1].map(toFixed)])
   })
 
-  it('should interpolate 2', () => {
+  it('should interpolate the example 2', () => {
     expect(
       mapSkippedPair(
         [0, 0.1, 0.3, 0.4, 0.5, 0.6, 0.7, 0.9],
@@ -25,7 +25,7 @@ describe('mapSkippedPair', () => {
     ])
   })
 
-  it('should interpolate 3', () => {
+  it('should interpolate the example 3', () => {
     expect(
       mapSkippedPair([0, 0.25, 0.75], [false, true, true], digits),
     ).toEqual([
@@ -34,7 +34,7 @@ describe('mapSkippedPair', () => {
     ])
   })
 
-  it('should interpolate 4', () => {
+  it('should interpolate the example 4', () => {
     expect(
       mapSkippedPair(
         [0, 0.1, 0.3, 0.4, 0.5, 0.6, 0.7, 0.9],
@@ -45,5 +45,16 @@ describe('mapSkippedPair', () => {
       [0, 1 / 3, 1 / 3, 2 / 3, 2 / 3, 1].map(toFixed),
       [0, 0.1, 0.4, 0.5, 0.5, 0.6].map(toFixed),
     ])
+  })
+
+  it('should return empty arrays for empty arrays', () => {
+    expect(mapSkippedPair([], [], digits)).toEqual([
+      [].map(toFixed),
+      [].map(toFixed),
+    ])
+  })
+
+  it('should throw if input and output arrays have different length', () => {
+    expect(() => mapSkippedPair([0], [true, true], digits)).toThrow()
   })
 })
