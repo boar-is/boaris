ea657631f8b0090330d44520590e581ccb644d29
Boris Zubchenko <boriszubc@gmail.com>
2024-10-13 00:32:15 +0300

    backup

--- src/app/[projectSlug]/[postSlug]/page.client.tsx
+++ src/app/[projectSlug]/[postSlug]/page.client.tsx
@@ -1,11 +1,11 @@
 'use client'
 
 import { Memo } from '@legendapp/state/react'
-import { useMotionValueEvent } from 'framer-motion'
-import { useEffect, useRef, useState } from 'react'
+import { useResizeObserver } from '@react-aria/utils'
+import { useMotionValueEvent, useScroll } from 'framer-motion'
+import { useEffect, useRef } from 'react'
 import { useWindowSize } from 'usehooks-ts'
 import { useWorkspaceProjectPostContext } from '~/src/app/[projectSlug]/[postSlug]/context'
-import { useScrollYProgress } from '~/src/hooks/use-scroll-y-progress'
 
 export function WorkspaceProjectClientPage() {
   const state$ = useWorkspaceProjectPostContext()
@@ -13,17 +13,19 @@ export function WorkspaceProjectClientPage() {
   const scrollableRef = useRef<HTMLDivElement | null>(null)
   const contentRef = useRef<HTMLDivElement | null>(null)
 
-  const scrollYProgress = useScrollYProgress({ ref: scrollableRef })
+  const { scrollYProgress } = useScroll({ target: scrollableRef })
   useMotionValueEvent(scrollYProgress, 'change', (progress) =>
     state$.scrollYProgress.set(progress ?? 0),
   )
+  useResizeObserver({
+    ref: scrollableRef,
+    onResize: () => window.scrollTo({ top: window.scrollY + 1 }),
+  })
 
   const { width } = useWindowSize({
     debounceDelay: 250,
   })
   useEffect(() => state$.windowWidth.set(width), [state$, width])
-
-  const [height, setHeight] = useState(100)
 
   return (
     <div>
@@ -31,21 +33,9 @@ export function WorkspaceProjectClientPage() {
         <div>
           <Memo>{state$.scrollYProgress}</Memo>
         </div>
-        <div>
-          <button type="button" onClick={() => setHeight(4000)}>
-            button
-          </button>
-          <button type="button" onClick={() => window.scrollTo({ top: 1 })}>
-            button
-          </button>
-        </div>
       </div>
-      <div
-        className="h-[400dvh] bg-gray-12/5"
-        ref={scrollableRef}
-        style={{ height }}
-      >
-        2
+      <div className="h-[400dvh] bg-gray-12/5" ref={scrollableRef}>
+        ABC
       </div>
     </div>
   )
