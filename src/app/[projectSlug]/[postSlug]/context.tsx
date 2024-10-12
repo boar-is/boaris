74202025a94afcc4295e7985375aa618da6a5d3b
Boris Zubchenko <boriszubc@gmail.com>
2024-10-12 20:55:40 +0300

    backup

--- src/app/[projectSlug]/[postSlug]/context.tsx
+++ src/app/[projectSlug]/[postSlug]/context.tsx
@@ -1,31 +1,11 @@
 'use client'
 
-import type { Observable, OpaqueObject } from '@legendapp/state'
+import type { Observable } from '@legendapp/state'
 import { useObservable } from '@legendapp/state/react'
-import type { JSONContent } from '@tiptap/react'
 import type { PropsWithChildren } from 'react'
 import { createContext } from '~/src/lib/react/create-context'
 
-export type BlogPostState = {
-  title: string
-  description: string
-  lead?: string | undefined
-  thumbnailSrc?: string | undefined
-  captions?:
-    | {
-        content: OpaqueObject<JSONContent>
-        mapping: {
-          input: Array<number>
-          output: Array<number>
-        }
-      }
-    | undefined
-  layouts?:
-    | {
-        primaryValue: LayoutValue
-      }
-    | undefined
-}
+const mapToContextValue = () => ({})
 
 export const [BlogPostContext, useBlogPostContext] = createContext<
   Observable<BlogPostState>
@@ -33,7 +13,7 @@ export const [BlogPostContext, useBlogPostContext] = createContext<
   name: 'BlogPostContext',
 })
 
-export function BlogPostProvider({ children }: PropsWithChildren) {
+export function WorkspaceProjectPostProvider({ children }: PropsWithChildren) {
   const state$ = useObservable<BlogPostState>({
     title: '1',
   })
