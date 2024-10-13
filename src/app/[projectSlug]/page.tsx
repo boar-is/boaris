efa4b65f071a38636ab86bf4b7b0e203c4fcaab9
Boris Zubchenko <boriszubc@gmail.com>
2024-10-13 18:38:12 +0300

    backup

--- src/app/[projectSlug]/page.tsx
+++ src/app/[projectSlug]/page.tsx
@@ -26,48 +26,44 @@ export default async function WorkspaceProjectPage({
   const { project, posts } = data
 
   return (
-    <article className="container flex flex-col gap-6 md:gap-10 items-stretch">
+    <article className="container flex flex-col gap-6 lg:gap-10 items-stretch">
       <header className="sr-only">
         <h1>{project.name}</h1>
       </header>
       {posts.length ? (
-        <div className="flex flex-row flex-wrap items-center w-full gap-8">
-          {Array.replicate(ensureDefined(posts[0]), 1).map((post) => (
-            <Link
-              key={post.slug}
-              href={`/${project.slug}/${post.slug}`}
-              className="max-w-prose"
-            >
-              <article className="group rounded-xl md:rounded-3xl flex flex-col justify-between items-center border border-gray-3 overflow-hidden transition-colors bg-gradient-to-tr from-gray-1/90 to-gray-2/90">
+        <div className="flex flex-col gap-8">
+          {Array.replicate(ensureDefined(posts[0]), 10).map((post) => (
+            <Link key={post.slug} href={`/${project.slug}/${post.slug}`}>
+              <article className="group rounded-xl lg:rounded-3xl flex flex-col lg:flex-row gap-4 lg:gap-8 p-6 justify-between items-center border border-gray-3 overflow-hidden transition-colors bg-gradient-to-tr from-gray-1/90 to-gray-2/90">
                 {post.thumbnailUrl && (
-                  <aside className="relative">
+                  <aside className="relative lg:max-w-sm">
                     <Image
                       src={post.thumbnailUrl}
                       alt={`${post.title}'s thumbnail`}
                       width={1024}
                       height={768}
-                      className="object-cover sizes-full"
+                      className="object-cover sizes-full rounded-xl lg:rounded-2xl"
                     />
                   </aside>
                 )}
-                <section className="flex-1 flex flex-col gap-3 md:gap-4 p-6">
+                <section className="flex-1 flex flex-col gap-3 lg:gap-4">
                   <header>
                     <hgroup>
-                      <h3 className="text-2xl md:text-3xl font-medium tracking-tight text-gray-12 text-balance">
+                      <h3 className="text-2xl lg:text-3xl font-medium tracking-tight text-gray-12 text-balance">
                         {post.title}
                       </h3>
                     </hgroup>
                   </header>
 
                   {post.authors.length && (
-                    <ul className="space-y-1 md:space-y-2 text-gray-8 text-sm md:text-base font-semibold tracking-tight">
+                    <ul className="space-y-1 lg:space-y-2 text-gray-8 text-sm lg:text-base font-semibold tracking-tight">
                       {post.authors.map((author) => (
                         <li
                           key={author.slug}
-                          className="flex items-center gap-1.5 md:gap-2"
+                          className="flex items-center gap-1.5 lg:gap-2"
                         >
                           {author.avatarUrl && (
-                            <aside className="relative size-6 md:size-8 rounded-full overflow-hidden border shadow-inner">
+                            <aside className="relative size-6 lg:size-8 rounded-full overflow-hidden border shadow-inner">
                               <Image
                                 src={author.avatarUrl}
                                 alt={`${author.name}'s avatar`}
@@ -83,30 +79,25 @@ export default async function WorkspaceProjectPage({
                     </ul>
                   )}
 
-                  {post.tags.length && (
-                    <ul className="flex flex-wrap gap-1 md:gap-1.5 text-xs md:text-sm font-medium tracking-wide text-gray-8 *:my-0.5">
-                      {post.tags.map((tag) => (
-                        <li key={tag.slug}>
-                          <span className="border border-gray-7 rounded-full px-3 py-0.5">
-                            {tag.name}
-                          </span>
-                        </li>
-                      ))}
-                    </ul>
-                  )}
-
                   <p className="text-gray-10 text-pretty !leading-relaxed max-w-prose">
                     {post.lead}
                   </p>
 
-                  <small className="text-gray-8 font-medium tracking-wide uppercase text-xs md:text-sm">
-                    {post.date}
-                  </small>
-
-                  <footer className="mt-auto">
-                    <div className="block font-semibold text-sm md:text-base py-2 text-center border border-gray-4 rounded-md md:rounded-2xl text-gray-10 bg-gray-2 group-hover:bg-gray-3 transition-colors">
-                      Read More
-                    </div>
+                  <footer className="flex justify-between gap-8 items-center">
+                    {post.tags.length && (
+                      <ul className="flex flex-wrap gap-1 lg:gap-1.5 text-xs lg:text-sm font-medium tracking-wide text-gray-8 *:my-0.5">
+                        {post.tags.map((tag) => (
+                          <li key={tag.slug}>
+                            <span className="border border-gray-7 rounded-full px-3 py-0.5">
+                              {tag.name}
+                            </span>
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                    <small className="text-gray-8 font-medium tracking-wide uppercase text-xs lg:text-sm">
+                      {post.date}
+                    </small>
                   </footer>
                 </section>
               </article>
@@ -114,7 +105,7 @@ export default async function WorkspaceProjectPage({
           ))}
         </div>
       ) : (
-        <div className="text-center text-2xl md:text-5xl text-gray-8 font-semibold capitalize">
+        <div className="text-center text-2xl lg:text-5xl text-gray-8 font-semibold capitalize">
           No posts yet
         </div>
       )}
