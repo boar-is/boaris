bf65884fa9d668cca0ee006c54c4ece1770963d3
Boris Zubchenko <boriszubc@gmail.com>
2024-10-13 19:16:36 +0300

    backup

--- src/app/[projectSlug]/page.tsx
+++ src/app/[projectSlug]/page.tsx
@@ -32,7 +32,7 @@ export default async function WorkspaceProjectPage({
         <div className="flex flex-col gap-8">
           {posts.map((post) => (
             <Link key={post.slug} href={`/${project.slug}/${post.slug}`}>
-              <article className="group rounded-xl lg:rounded-3xl flex flex-col lg:flex-row gap-4 lg:gap-8 p-6 justify-between items-center border border-gray-3 overflow-hidden transition-colors bg-gradient-to-tr from-gray-1/90 to-gray-2/90">
+              <article className="group rounded-xl lg:rounded-3xl flex flex-col lg:flex-row gap-4 lg:gap-8 p-4 lg:p-6 justify-between items-center border border-gray-3 overflow-hidden transition-colors bg-gradient-to-tr from-gray-1/90 to-gray-2/90">
                 {post.thumbnailUrl && (
                   <aside className="relative lg:basis-1/2 xl:basis-1/3">
                     <Image
@@ -46,36 +46,35 @@ export default async function WorkspaceProjectPage({
                 )}
                 <section className="flex-1 flex flex-col gap-3 lg:gap-4">
                   <header>
-                    <hgroup>
+                    <hgroup className="space-y-1">
                       <h3 className="text-2xl lg:text-3xl font-medium tracking-tight text-gray-12 text-balance">
                         {post.title}
                       </h3>
+                      {post.authors.length && (
+                        <ul className="space-y-1 lg:space-y-2 text-gray-8 text-sm lg:text-base font-semibold tracking-tight">
+                          {post.authors.map((author) => (
+                            <li
+                              key={author.slug}
+                              className="flex items-center gap-1.5 lg:gap-2"
+                            >
+                              {author.avatarUrl && (
+                                <aside className="relative size-6 lg:size-8 rounded-full overflow-hidden border shadow-inner">
+                                  <Image
+                                    src={author.avatarUrl}
+                                    alt={`${author.name}'s avatar`}
+                                    width={32}
+                                    height={32}
+                                    className="size-full object-cover shadow-inner"
+                                  />
+                                </aside>
+                              )}
+                              {author.name}
+                            </li>
+                          ))}
+                        </ul>
+                      )}
                     </hgroup>
                   </header>
-
-                  {post.authors.length && (
-                    <ul className="space-y-1 lg:space-y-2 text-gray-8 text-sm lg:text-base font-semibold tracking-tight">
-                      {post.authors.map((author) => (
-                        <li
-                          key={author.slug}
-                          className="flex items-center gap-1.5 lg:gap-2"
-                        >
-                          {author.avatarUrl && (
-                            <aside className="relative size-6 lg:size-8 rounded-full overflow-hidden border shadow-inner">
-                              <Image
-                                src={author.avatarUrl}
-                                alt={`${author.name}'s avatar`}
-                                width={32}
-                                height={32}
-                                className="size-full object-cover shadow-inner"
-                              />
-                            </aside>
-                          )}
-                          {author.name}
-                        </li>
-                      ))}
-                    </ul>
-                  )}
 
                   {post.lead && (
                     <p className="text-gray-10 text-pretty !leading-relaxed max-w-prose">
