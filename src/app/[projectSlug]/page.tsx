fffc81568c534d40230fe34d26997915d57778be
Boris Zubchenko <boriszubc@gmail.com>
2024-10-13 18:07:38 +0300

    backup

--- src/app/[projectSlug]/page.tsx
+++ src/app/[projectSlug]/page.tsx
@@ -29,84 +29,82 @@ export default async function WorkspaceProjectPage({
         <h1>{project.name}</h1>
       </header>
       {posts.length ? (
-        posts.map((post) => (
-          <Link
-            key={post.slug}
-            href={`/${project.slug}/${post.slug}`}
-            className="group rounded-xl md:rounded-3xl"
-          >
-            <article className="flex flex-col justify-between md:group-even:flex-row md:flex-row-reverse border border-gray-3 rounded-[inherit] overflow-hidden transition-colors bg-gradient-to-tr from-gray-1 to-gray-2">
-              {post.thumbnailUrl && (
-                <aside className="relative basis-2/5 aspect-video">
-                  <Image
-                    src={post.thumbnailUrl}
-                    alt={`${post.title}'s thumbnail`}
-                    width={1024}
-                    height={768}
-                    className="object-cover size-full"
-                  />
-                </aside>
-              )}
-              <section className="flex-1 flex flex-col gap-3 md:gap-6 p-4 md:p-10">
-                <header>
-                  <hgroup>
-                    <small className="text-gray-8 font-bold tracking-wide uppercase text-xs md:text-sm">
-                      {post.date}
-                    </small>
-                    <h3 className="text-2xl md:text-4xl font-semibold tracking-tight text-gray-12 text-balance">
-                      {post.title}
-                    </h3>
-                  </hgroup>
-                </header>
-                {post.tags.length && (
-                  <ul className="flex flex-wrap gap-1 md:gap-1.5 text-xs md:text-sm font-medium tracking-wide text-gray-8 *:my-0.5">
-                    {post.tags.map((tag) => (
-                      <li key={tag.slug}>
-                        <span className="border border-gray-7 rounded-full px-3 py-0.5">
-                          {tag.name}
-                        </span>
-                      </li>
-                    ))}
-                  </ul>
+        <div className="flex flex-col max-w-2xl mx-auto items-center">
+          {posts.map((post) => (
+            <Link key={post.slug} href={`/${project.slug}/${post.slug}`}>
+              <article className="group rounded-xl md:rounded-3xl flex flex-col justify-between items-center border border-gray-3 overflow-hidden transition-colors bg-gradient-to-tr from-gray-1/90 to-gray-2/90">
+                {post.thumbnailUrl && (
+                  <aside className="relative">
+                    <Image
+                      src={post.thumbnailUrl}
+                      alt={`${post.title}'s thumbnail`}
+                      width={1024}
+                      height={768}
+                      className="object-cover sizes-full"
+                    />
+                  </aside>
                 )}
+                <section className="flex-1 flex flex-col gap-3 md:gap-6 p-6">
+                  <header>
+                    <hgroup>
+                      <small className="text-gray-8 font-bold tracking-wide uppercase text-xs md:text-sm">
+                        {post.date}
+                      </small>
+                      <h3 className="text-2xl md:text-4xl font-semibold tracking-tight text-gray-12 text-balance">
+                        {post.title}
+                      </h3>
+                    </hgroup>
+                  </header>
+                  {post.tags.length && (
+                    <ul className="flex flex-wrap gap-1 md:gap-1.5 text-xs md:text-sm font-medium tracking-wide text-gray-8 *:my-0.5">
+                      {post.tags.map((tag) => (
+                        <li key={tag.slug}>
+                          <span className="border border-gray-7 rounded-full px-3 py-0.5">
+                            {tag.name}
+                          </span>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
 
-                <p className="text-gray-10 font-medium text-pretty text-sm md:text-base !leading-relaxed max-w-prose">
-                  {post.lead}
-                </p>
+                  <p className="text-gray-10 font-medium text-pretty text-sm md:text-base !leading-relaxed max-w-prose">
+                    {post.lead}
+                  </p>
 
-                {post.authors.length && (
-                  <ul className="space-y-1 md:space-y-2 text-gray-8 text-sm md:text-base font-semibold tracking-tight">
-                    {post.authors.map((author) => (
-                      <li
-                        key={author.slug}
-                        className="flex items-center gap-1.5 md:gap-2"
-                      >
-                        {author.avatarUrl && (
-                          <aside className="relative size-6 md:size-8 rounded-full overflow-hidden border shadow-inner">
-                            <Image
-                              src={author.avatarUrl}
-                              alt={`${author.name}'s avatar`}
-                              width={32}
-                              height={32}
-                              className="size-full object-cover shadow-inner"
-                            />
-                          </aside>
-                        )}
-                        {author.name}
-                      </li>
-                    ))}
-                  </ul>
-                )}
+                  {post.authors.length && (
+                    <ul className="space-y-1 md:space-y-2 text-gray-8 text-sm md:text-base font-semibold tracking-tight">
+                      {post.authors.map((author) => (
+                        <li
+                          key={author.slug}
+                          className="flex items-center gap-1.5 md:gap-2"
+                        >
+                          {author.avatarUrl && (
+                            <aside className="relative size-6 md:size-8 rounded-full overflow-hidden border shadow-inner">
+                              <Image
+                                src={author.avatarUrl}
+                                alt={`${author.name}'s avatar`}
+                                width={32}
+                                height={32}
+                                className="size-full object-cover shadow-inner"
+                              />
+                            </aside>
+                          )}
+                          {author.name}
+                        </li>
+                      ))}
+                    </ul>
+                  )}
 
-                <footer className="mt-auto">
-                  <div className="block font-semibold text-sm md:text-base py-2 md:py-3 text-center border border-gray-4 rounded-md md:rounded-2xl text-gray-10 bg-gray-2 group-hover:bg-gray-3 transition-colors">
-                    Read More
-                  </div>
-                </footer>
-              </section>
-            </article>
-          </Link>
-        ))
+                  <footer className="mt-auto">
+                    <div className="block font-semibold text-sm md:text-base py-2 md:py-3 text-center border border-gray-4 rounded-md md:rounded-2xl text-gray-10 bg-gray-2 group-hover:bg-gray-3 transition-colors">
+                      Read More
+                    </div>
+                  </footer>
+                </section>
+              </article>
+            </Link>
+          ))}
+        </div>
       ) : (
         <div className="text-center text-2xl md:text-5xl text-gray-8 font-semibold capitalize">
           No posts yet
