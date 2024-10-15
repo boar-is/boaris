986108f9523aedffa675e2de7ff069d6ebba911d
Boris Zubchenko <boriszubc@gmail.com>
2024-10-15 21:09:13 +0300

    backup

---
+++ model/interpolation.ts
@@ -0,0 +1,57 @@
+import { toFixedNumber } from './number/to-fixed-number'
+import { ensureDefined } from './unknown/ensure-defined'
+
+export type Interpolation<T = number> = {
+  input: Array<number>
+  output: Array<T>
+}
+
+export const remappedFalsyOutput = (
+  { input, output }: Interpolation<unknown>,
+  digits = 5,
+): Interpolation<number> => {
+  if (input.length !== output.length) {
+    throw new Error('Inputs and output arrays must be of the same length')
+  }
+
+  let trueRatio = 0
+  for (let i = 0; i < input.length; i++) {
+    if (!output[i]) {
+      continue
+    }
+
+    const start = ensureDefined(input[i])
+    const end = input[i + 1]
+
+    trueRatio += (end ?? 1) - start
+  }
+
+  const multiplier = 1 / trueRatio
+
+  const toFixed = toFixedNumber()(digits)
+
+  const remappedInput: Array<number> = []
+  const remappedOutput: Array<number> = []
+
+  let startingRatio = 0
+  for (let i = 0; i < input.length; i++) {
+    if (output[i] === false) {
+      continue
+    }
+
+    const start = ensureDefined(input[i])
+    const end = input[i + 1] ?? 1
+
+    const mappedInputEnd = startingRatio + (end - start) * multiplier
+
+    remappedInput.push(toFixed(startingRatio), toFixed(mappedInputEnd))
+    remappedOutput.push(toFixed(start), toFixed(end))
+
+    startingRatio = mappedInputEnd
+  }
+
+  return {
+    input: remappedInput,
+    output: remappedOutput,
+  }
+}
