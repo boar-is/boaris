b41b7d6fd1dafbf1d390e2b477a26279a6dc7b66
Boris Zubchenko <boriszubc@gmail.com>
2024-10-15 17:40:29 +0300

    backup

--- convex/values/revisions/layouts/layouts.ts
+++ convex/values/revisions/layouts/layouts.ts
@@ -1,22 +1,10 @@
 import { v } from 'convex/values'
-import { layoutMode } from './layoutMode'
+import { layoutOverride } from './layoutOverride'
 import { layoutPrimary } from './layoutPrimary'
 
 export const layouts = v.object({
   primary: layoutPrimary,
-  overrides: v.optional(
-    v.array(
-      v.object({
-        modes: v.optional(v.array(layoutMode)),
-        minWidthPx: v.optional(v.number()),
-        disabled: v.optional(v.boolean()),
-        /**
-         * @see Delta
-         */
-        changesDelta: v.any(),
-      }),
-    ),
-  ),
+  overrides: v.optional(v.array(layoutOverride)),
 })
 
 export type Layouts = typeof layouts.type
