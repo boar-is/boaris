fe454995a60301580fe7683e883b59e488bbe80a
Boris Zubchenko <boriszubc@gmail.com>
2024-10-15 20:20:06 +0300

    backup

---
+++ model/layout/interpolate-skipped.ts
@@ -0,0 +1,54 @@
+import type { Interpolation } from '~/convex/values/_shared/interpolation'
+import { toFixedNumber } from '~/model/number/to-fixed-number'
+import { ensureDefined } from '~/model/unknown/ensure-defined'
+
+export const interpolateSkipped = (
+  changes: Array<{ at: number; value?: unknown | undefined }>,
+  digits = 5,
+): Interpolation => {
+  let trueRatio = 0
+
+  for (let i = 0; i < changes.length; i++) {
+    const { at, value } = ensureDefined(changes[i])
+
+    if (!value) {
+      continue
+    }
+
+    const start = ensureDefined(at)
+    const end = changes[i + 1]?.at
+
+    trueRatio += (end ?? 1) - start
+  }
+
+  const multiplier = 1 / trueRatio
+
+  const toFixed = toFixedNumber()(digits)
+
+  const input: Array<number> = []
+  const output: Array<number> = []
+
+  let startingRatio = 0
+  for (let i = 0; i < changes.length; i++) {
+    const { at, value } = ensureDefined(changes[i])
+
+    if (!value) {
+      continue
+    }
+
+    const start = ensureDefined(at)
+    const end = changes[i + 1]?.at ?? 1
+
+    const mappedInputEnd = startingRatio + (end - start) * multiplier
+
+    input.push(toFixed(startingRatio), toFixed(mappedInputEnd))
+    output.push(toFixed(start), toFixed(end))
+
+    startingRatio = mappedInputEnd
+  }
+
+  return {
+    input,
+    output,
+  }
+}
