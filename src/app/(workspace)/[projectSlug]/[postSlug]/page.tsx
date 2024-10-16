a4d6d2f01dc172409717336045f2f83d4cd873dd
Boris Zubchenko <boriszubc@gmail.com>
2024-10-16 20:00:20 +0300

    backup

---
+++ src/app/(workspace)/[projectSlug]/[postSlug]/page.tsx
@@ -0,0 +1,106 @@
+import { fetchQuery } from 'convex/nextjs'
+import { notFound } from 'next/navigation'
+import { api } from '~/convex/_generated/api'
+import { currentWorkspaceSlug } from '~/lib/constants'
+import { Image } from '~/lib/media/image'
+import type { PropsWithStaticParams } from '~/lib/react/props-with-static-params'
+import { getMonoFontVariable } from '~/lib/style/fonts/get-mono-font-variable'
+import { cx } from '~/lib/utils/cx'
+
+export async function generateStaticParams() {
+  return fetchQuery(api.queries.postParams.default)
+}
+
+export default async function WorkspaceProjectPostPage({
+  params: { workspaceSlug = currentWorkspaceSlug, projectSlug, postSlug },
+}: PropsWithStaticParams<typeof generateStaticParams>) {
+  const data = await fetchQuery(api.queries.postPage.default, {
+    workspaceSlug,
+    projectSlug,
+    postSlug,
+  })
+
+  if (!data) {
+    notFound()
+  }
+
+  return (
+    <article
+      className={cx(
+        getMonoFontVariable(),
+        'container flex flex-col gap-12 items-center',
+      )}
+    >
+      <header className="w-full max-w-prose">
+        <hgroup className="flex flex-col gap-6">
+          {data.post.thumbnailUrl && (
+            <figure className="relative">
+              <Image
+                src={data.post.thumbnailUrl}
+                alt={`${data.post.title}'s thumbnail's blur`}
+                width={1024}
+                height={768}
+                className="absolute rounded-2xl blur-2xl opacity-35 pointer-events-none"
+              />
+              <Image
+                src={data.post.thumbnailUrl}
+                alt={`${data.post.title}'s thumbnail`}
+                width={1024}
+                height={768}
+                className="rounded-2xl drop-shadow-xl"
+              />
+            </figure>
+          )}
+          <h1 className="text-4xl text-gray-12 font-semibold tracking-tight text-balance">
+            {data.post.title}
+          </h1>
+          {data.tags.length && (
+            <ul className="flex flex-wrap gap-1.5 lg:gap-2 text-sm lg:text-base font-medium tracking-wide text-gray-10 *:my-0.5">
+              {data.tags.map((tag) => (
+                <li key={tag.slug}>
+                  <span className="border border-gray-9 rounded-full px-3 py-0.5">
+                    {tag.name}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          )}
+          {data.post.lead && (
+            <p className="text-gray-11 text-pretty text-lg font-medium">
+              {data.post.lead}
+            </p>
+          )}
+          <div className="flex justify-between gap-8 items-center">
+            {data.authors.length && (
+              <ul className="space-y-1 lg:space-y-2 text-gray-10 lg:text-lg font-medium tracking-tight">
+                {data.authors.map((author) => (
+                  <li
+                    key={author.slug}
+                    className="flex items-center gap-1.5 lg:gap-2"
+                  >
+                    {author.avatarUrl && (
+                      <aside className="relative size-8 lg:size-10 rounded-full overflow-hidden border shadow-inner">
+                        <Image
+                          src={author.avatarUrl}
+                          alt={`${author.name}'s avatar`}
+                          width={32}
+                          height={32}
+                          className="size-full object-cover shadow-inner"
+                        />
+                      </aside>
+                    )}
+                    {author.name}
+                  </li>
+                ))}
+              </ul>
+            )}
+            <small className="text-gray-10 font-medium tracking-wide text-sm lg:text-base">
+              {data.post.date}
+            </small>
+          </div>
+        </hgroup>
+      </header>
+      <hr className="w-full max-w-prose border-gray-3" />
+    </article>
+  )
+}
