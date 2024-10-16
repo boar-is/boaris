e641d85e3c585309707032345f371f98da94b696
Boris Zubchenko <boriszubc@gmail.com>
2024-10-16 19:56:45 +0300

    backup

--- src/app/(workspace)/page.tsx
+++ src/app/(workspace)/page.tsx
@@ -1,7 +1,7 @@
 import { fetchQuery } from 'convex/nextjs'
-import { currentWorkspaceSlug } from '~/constants'
 import { api } from '~/convex/_generated/api'
-import type { PropsWithStaticParams } from '~/src/lib/react/props-with-static-params'
+import { currentWorkspaceSlug } from '~/lib/constants'
+import type { PropsWithStaticParams } from '~/lib/react/props-with-static-params'
 
 export async function generateStaticParams() {
   return fetchQuery(api.queries.workspaceParams.default)
---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { PropsWithChildren } from 'react'
+import { FramerMotionProvider } from '~/lib/framer-motion/provider'
+import { I18nProvider } from '~/lib/i18n/i18n-provider'
+import { LocalizedStringProvider } from '~/lib/i18n/localized-string-provider'
+import { FocusVisibleProvider } from '~/lib/interactions/focus-visible-provider'
+import { AriaRouterProvider } from '~/lib/routing/aria-router-provider'
+import { getSansFontVariable } from '~/lib/style/fonts/get-sans-font-variable'
+import { ToastProvider } from '~/lib/toast/toast-provider'
+
+export default function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <html lang="en" className={getSansFontVariable()}>
+      <body className="dark flex flex-col gap-4 md:gap-10 items-stretch min-h-dvh bg-gray-1 font-sans text-gray-11 antialiased">
+        <FocusVisibleProvider />
+        <LocalizedStringProvider locale="en-US" />
+        <ToastProvider />
+        <I18nProvider>
+          <AriaRouterProvider>
+            <FramerMotionProvider>{children}</FramerMotionProvider>
+          </AriaRouterProvider>
+        </I18nProvider>
+      </body>
+    </html>
+  )
+}
