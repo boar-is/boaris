d9446f82dff7d810c40072e3dad4e008465ac295
Boris Zubchenko <boriszubc@gmail.com>
2024-10-20 22:44:07 +0300

    backup

---
+++ src/data/_shared/json-content.ts
@@ -0,0 +1,3 @@
+import * as S from '@effect/schema/Schema'
+
+export const JsonContent = S.Any
--- src/data/post/post-captions.ts
+++ src/data/post/post-captions.ts
@@ -1,7 +1,8 @@
 import * as S from '@effect/schema/Schema'
 import { Interpolation } from '~/data/_shared/interpolation'
+import { JsonContent } from '~/data/_shared/json-content'
 
 export class PostCaptions extends S.Class<PostCaptions>('PostCaptions')({
-  content: S.Any,
+  content: JsonContent,
   interpolation: S.OptionFromUndefinedOr(Interpolation),
 }) {}
