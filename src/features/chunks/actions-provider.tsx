ce643250d711b71c3738714214371b6cb09e3931
Boris Zubchenko <boriszubc@gmail.com>
2024-10-20 15:23:33 +0300

    backup

---
+++ src/features/chunks/actions-provider.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import type { Observable } from '@legendapp/state'
+import { useObservable } from '@legendapp/state/react'
+import type { PropsWithChildren } from 'react'
+import type { Action } from '~/convex/values/chunks/action'
+import type { Chunk } from '~/convex/values/chunks/chunk'
+import { createStrictContext } from '~/lib/react/create-strict-context'
+
+export type ActionsContextValue = {
+  actions: Record<string, Array<Action>>
+}
+
+export const [ActionsContext, useActions$] = createStrictContext<
+  Observable<ActionsContextValue>
+>({
+  name: 'ActionsContext',
+})
+
+export function ActionsProvider({
+  children,
+  chunks$,
+}: PropsWithChildren & {
+  chunks$: Observable<Array<Chunk>>
+}) {
+  const actions$ = useObservable((): ActionsContextValue => {
+    const chunks = chunks$.get(true)
+    return {
+      actions: chunks.reduce(
+        (actions: ActionsContextValue['actions'], chunk) => {
+          for (const [trackId, trackActions] of Object.entries(chunk.actions)) {
+            const offsetTrackActions = trackActions.map((it) => ({
+              ...it,
+              offset: it.offset + chunk.offset,
+            }))
+            actions[trackId] =
+              actions[trackId]?.concat(offsetTrackActions) ?? offsetTrackActions
+          }
+
+          return actions
+        },
+        {},
+      ),
+    }
+  })
+
+  return (
+    <ActionsContext.Provider value={actions$}>
+      {children}
+    </ActionsContext.Provider>
+  )
+}
