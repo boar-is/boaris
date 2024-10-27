7cb3d65e372aa2b821f3c1140578262c58d2e802
Boris Zubchenko <boriszubc@gmail.com>
2024-10-28 01:34:27 +0200

    backup

--- src/model/action.ts
+++ src/model/action.ts
@@ -1,6 +1,16 @@
+import type {
+  ChangeSet,
+  EditorSelection,
+  EditorState,
+} from '@uiw/react-codemirror'
 import { type Infer, v } from 'convex/values'
 import { Match, Schema } from 'effect'
-import { ActionSelect, actionSelect } from '~/model/actionSelect'
+import { changeSetFromActionInsertSpec } from '~/model/actionInsertSpec'
+import {
+  ActionSelect,
+  actionSelect,
+  editorSelectionFromActionSelect,
+} from '~/model/actionSelect'
 import { ActionInsert, actionInsert } from './actionInsert'
 
 export const action = v.union(actionInsert, actionSelect)
@@ -14,3 +24,56 @@ export const actionEncodedFromEntity = (
     Match.when({ type: 'select' }, ActionSelect.encodedFromEntity),
     Match.exhaustive,
   )
+
+export const seekCodeMirrorActions =
+  (state: EditorState) =>
+  (actions: ReadonlyArray<typeof Action.Type>) =>
+  (initialState: string) =>
+  (anchor: number | undefined, head: number | undefined) => {
+    if (!head) {
+      return {
+        changes: {
+          from: 0,
+          to: state.doc.length,
+          insert: initialState,
+        },
+        selection: undefined,
+        scrollIntoView: true,
+      }
+    }
+
+    let changes: ChangeSet | undefined
+    let selection: EditorSelection | undefined
+
+    const applyAction = (action: typeof Action.Type, inverted = false) => {
+      switch (action.type) {
+        case 'insert': {
+          const spec = inverted ? action.backwardSpec : action.forwardSpec
+          const changeSet = changeSetFromActionInsertSpec(spec)
+          changes = changes ? changes.compose(changeSet) : changeSet
+          break
+        }
+        case 'select': {
+          selection = editorSelectionFromActionSelect(action)
+          break
+        }
+      }
+    }
+
+    if (!anchor || anchor < head) {
+      for (let i = anchor ? anchor + 1 : 0; i <= head; i++) {
+        applyAction(actions[i]!)
+      }
+    } else if (anchor > head) {
+      for (let i = anchor; i > head; i--) {
+        applyAction(actions[i]!, true)
+      }
+    }
+
+    return {
+      changes,
+      selection,
+      scrollIntoView: true,
+      sequential: true,
+    }
+  }
