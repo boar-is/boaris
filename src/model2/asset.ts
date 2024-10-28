077811aa5b853626985cbaa63a3a20292da12fbf
Boris Zubchenko <boriszubc@gmail.com>
2024-10-28 20:38:32 +0200

    backup

---
+++ src/model2/asset.ts
@@ -0,0 +1,31 @@
+import { type Infer, v } from 'convex/values'
+import { Match, Schema } from 'effect'
+
+import type { PropsWithGetUrl } from '~/lib/utils/props-with-get-url'
+import { AssetImageDynamic, assetImageDynamic } from './assetImageDynamic'
+import { AssetImageStatic, assetImageStatic } from './assetImageStatic'
+import { AssetText, assetText } from './assetText'
+
+export const asset = v.union(assetImageDynamic, assetImageStatic, assetText)
+
+export const Asset = Schema.Union(
+  AssetImageDynamic,
+  AssetImageStatic,
+  AssetText,
+)
+export const assetEncodedFromEntity = async (
+  t: Infer<typeof asset>,
+  withGetUrl: PropsWithGetUrl,
+): Promise<typeof Asset.Encoded> =>
+  Match.value(t).pipe(
+    Match.when(
+      { type: 'image-dynamic' },
+      AssetImageDynamic.encodedFromEntity(withGetUrl),
+    ),
+    Match.when(
+      { type: 'image-static' },
+      AssetImageStatic.encodedFromEntity(withGetUrl),
+    ),
+    Match.when({ type: 'text' }, AssetText.encodedFromEntity),
+    Match.exhaustive,
+  )
