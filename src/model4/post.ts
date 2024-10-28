961bf62637fbe58d1d7a85ceb9ecae6a79a2739e
Boris Zubchenko <boriszubc@gmail.com>
2024-10-29 01:41:33 +0200

    bc

--- src/model4/post.ts
+++ src/model4/post.ts
@@ -1,13 +1,8 @@
 import { type Infer, v } from 'convex/values'
 import { Schema } from 'effect'
-import type { PropsWithGetUrl } from '~/lib/utils/props-with-get-url'
 
 export const post = v.object({
   slug: v.string(),
-  title: v.string(),
-  lead: v.optional(v.string()),
-  description: v.string(),
-  posterId: v.optional(v.id('_storage')),
   projectId: v.id('projects'),
   draftRevisionId: v.id('revisions'),
   publishedRevisionId: v.optional(v.id('revisions')),
@@ -15,29 +10,17 @@ export const post = v.object({
 
 export class Post extends Schema.Class<Post>('Post')({
   slug: Schema.NonEmptyTrimmedString,
-  title: Schema.NonEmptyTrimmedString,
-  lead: Schema.OptionFromUndefinedOr(Schema.NonEmptyTrimmedString),
-  description: Schema.NonEmptyTrimmedString,
-  posterUrl: Schema.OptionFromUndefinedOr(Schema.NonEmptyTrimmedString),
   date: Schema.DateFromNumber,
 }) {
-  static encodedFromEntity({ getUrl }: PropsWithGetUrl) {
-    return async ({
+  static encodedFromEntity({
+    slug,
+    _creationTime,
+  }: Infer<typeof post> & {
+    _creationTime: number
+  }): typeof Post.Encoded {
+    return {
       slug,
-      title,
-      lead,
-      description,
-      posterId,
-      _creationTime,
-    }: Infer<typeof post> & {
-      _creationTime: number
-    }): Promise<typeof Post.Encoded> => ({
-      slug,
-      title,
-      lead,
-      description,
-      posterUrl: posterId && (await getUrl(posterId)),
       date: _creationTime,
-    })
+    }
   }
 }
---
+++ src/model4/revision.ts
@@ -0,0 +1,29 @@
+import { type Infer, v } from 'convex/values'
+import { Schema } from 'effect'
+import type { PropsWithGetUrl } from '~/lib/utils/props-with-get-url'
+
+export const revision = v.object({
+  title: v.string(),
+  lead: v.optional(v.string()),
+  description: v.string(),
+  posterId: v.optional(v.id('_storage')),
+})
+
+export class Revision extends Schema.Class<Revision>('Revision')({
+  title: Schema.NonEmptyTrimmedString,
+  lead: Schema.OptionFromUndefinedOr(Schema.NonEmptyTrimmedString),
+  description: Schema.NonEmptyTrimmedString,
+  posterUrl: Schema.OptionFromUndefinedOr(Schema.NonEmptyTrimmedString),
+}) {
+  static async encodedFromEntity(
+    { title, lead, description, posterId }: Infer<typeof revision>,
+    { getUrl }: PropsWithGetUrl,
+  ): Promise<typeof Revision.Encoded> {
+    return {
+      title,
+      lead,
+      description,
+      posterUrl: posterId && (await getUrl(posterId)),
+    }
+  }
+}
