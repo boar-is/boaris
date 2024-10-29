f7fe12c3f146c74bed627b19335a7dd01d579c14
Boris Zubchenko <boriszubc@gmail.com>
2024-10-29 03:22:13 +0200

    bc

--- src/features/tracks-atom-context.tsx
+++
@@ -1,9 +0,0 @@
-import type { Atom } from 'jotai'
-import { createStrictContext } from '~/lib/react/create-strict-context'
-import type { Track } from '~/model/track'
-
-export const [TracksAtomContext, useTracksAtom] = createStrictContext<
-  Atom<ReadonlyArray<typeof Track.Type>>
->({
-  name: 'TracksAtomContext',
-})
