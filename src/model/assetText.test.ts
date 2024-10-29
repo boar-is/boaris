2c410dd1b708d62d2e50f989de4b4320cf519c27
Boris Zubchenko <boriszubc@gmail.com>
2024-10-30 00:34:05 +0200

    bc

--- src/model/assetText.test.ts
+++ src/model/assetText.test.ts
@@ -1,7 +1,17 @@
-import { describe, it } from 'vitest'
+import { Text } from '@uiw/react-codemirror'
+import { describe, expect, it } from 'vitest'
+import { reversedTextChanges } from '~/model/assetText'
 
 describe.concurrent('reversedTextChanges', () => {
-  it('should return an empty array for no changes', () => {
-    // const initialValue = Text.of()
+  it.concurrent.each<{
+    params: Parameters<typeof reversedTextChanges>
+    returns: ReturnType<typeof reversedTextChanges>
+  }>([
+    {
+      params: [Text.empty, []],
+      returns: [],
+    },
+  ])('$params -> $returns', ({ params, returns }) => {
+    expect(reversedTextChanges(...params)).toEqual(returns)
   })
 })
