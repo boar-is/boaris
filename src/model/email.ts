a5f79b6b55ee5381fb6a321ba4304ce35803e234
Boris Zubchenko <boriszubc@gmail.com>
2024-11-07 19:19:19 +0200

    bc

---
+++ src/model/email.ts
@@ -0,0 +1,7 @@
+import { Schema } from 'effect'
+
+export const Email = Schema.String.pipe(
+  Schema.pattern(
+    /^(?!\.)(?!.*\.\.)([A-Z0-9_+-.]*)[A-Z0-9_+-]@([A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i,
+  ),
+)
