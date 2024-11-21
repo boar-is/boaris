16b71d24b14033a18ecf902acc3482b901f4da90
Boaris <boriszubc@gmail.com>
2024-11-21 02:00:38 +0200

    bc

--- src/lib/buttons/button.tsx
+++ src/lib/buttons/button.tsx
@@ -1,30 +1,78 @@
 'use client'
 
-import type { HTMLAttributes } from 'react'
-import type { ButtonProps } from 'react-aria-components'
-import type { VariantProps } from 'tailwind-variants'
-import { buttonStyles } from './button-styles'
-
-export {
-  Button,
-  type ButtonProps,
-  ButtonContext,
-  ToggleButton,
-  ToggleButtonContext,
+import { forwardRef } from 'react'
+import {
+  Button as ButtonPrimitive,
+  type ButtonProps as ButtonPrimitiveProps,
 } from 'react-aria-components'
+import { type VariantProps, tv } from 'tailwind-variants'
+import { cr } from '~/lib/react/cr'
 
-export const getButtonProps = (
-  props: VariantProps<typeof buttonStyles> = {},
-): [buttonProps: ButtonProps, iconProps: HTMLAttributes<HTMLElement>] => {
-  const { root, icon } = buttonStyles(props)
-
-  return [
+export const buttonStyles = tv({
+  base: [
+    'relative isolate transition',
+    'inline-flex items-center justify-center',
+    'border font-medium no-underline cursor-default',
+  ],
+  variants: {
+    intent: {
+      primary: ['text-bg bg-fg border-transparent'],
+      secondary: [
+        'bg-secondary text-secondary-fg border-white/5 hover:bg-secondary/90 pressed:bg-secondary/80',
+        'after:absolute after:-inset-px after:-z-10 after:shadow-inset',
+      ],
+      tertiary: '',
+      destructive: '',
+    },
+    size: {
+      xs: 'gap-1',
+      sm: 'gap-1.5',
+      md: ['gap-2 min-h-10 px-4 py-2 text-base lg:text-sm', 'rounded-lg'],
+      lg: 'gap-2.5',
+    },
+    isDisabled: {
+      true: 'opacity-60',
+    },
+  },
+  compoundVariants: [
     {
-      type: 'button' as const,
-      className: root({}),
+      intent: 'secondary',
+      size: 'md',
+      className: 'after:rounded-lg',
     },
     {
-      className: icon(),
+      intent: 'secondary',
+      isDisabled: true,
+      className: 'after:shadow-none',
     },
-  ]
-}
+  ],
+  defaultVariants: {
+    intent: 'primary',
+    size: 'md',
+  },
+})
+
+export interface ButtonProps
+  extends ButtonPrimitiveProps,
+    VariantProps<typeof buttonStyles> {}
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  function Button({ className, intent, size, ...props }, ref) {
+    return (
+      <ButtonPrimitive
+        ref={ref}
+        {...props}
+        className={cr(className, (className, renderProps) =>
+          buttonStyles({
+            ...renderProps,
+            intent,
+            size,
+            className,
+          }),
+        )}
+      />
+    )
+  },
+)
+
+export { ButtonContext } from 'react-aria-components'
--- src/lib/react/ctr.ts
+++ src/lib/react/ctr.ts
@@ -1,9 +1,10 @@
+import type { ClassValue } from 'clsx'
 import { composeRenderProps } from 'react-aria-components'
 import { cx } from './cx'
 
 export const composeTailwindRenderProps = <T>(
   className: string | ((v: T) => string) | undefined,
-  tw: string | Array<string | undefined>,
-) => composeRenderProps(className, (cs) => cx(tw, cs))
+  ...inputs: Array<ClassValue>
+) => composeRenderProps(className, (cs) => cx(...inputs, cs))
 
 export { composeTailwindRenderProps as ctr }
