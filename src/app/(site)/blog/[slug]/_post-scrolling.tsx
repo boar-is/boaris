3287ef77187f45ad76a24ab22ad4146113e5ca8f
Boaris <boriszubc@gmail.com>
2024-11-27 06:41:44 +0200

    bc

--- src/app/(site)/blog/[slug]/_post-scrolling.tsx
+++ src/app/(site)/blog/[slug]/_post-scrolling.tsx
@@ -1,16 +1,36 @@
 import { type Editor, EditorContent, useEditor } from '@tiptap/react'
-import { Option, identity } from 'effect'
+import ReactCodeMirror, {
+  type BasicSetupOptions,
+  type ReactCodeMirrorRef,
+} from '@uiw/react-codemirror'
+import { Array, Match, Option, identity } from 'effect'
+import { jotai } from 'jotai-components'
 import { atomEffect } from 'jotai-effect'
-import { useAtomValue } from 'jotai/index'
-import { useRef } from 'react'
+import { type Atom, atom, useAtomValue } from 'jotai/index'
+import { splitAtom } from 'jotai/utils'
+import { AnimatePresence } from 'motion/react'
+import {
+  type PropsWithChildren,
+  forwardRef,
+  memo,
+  useMemo,
+  useRef,
+} from 'react'
 import {
   PlaybackProgressAtomContext,
   usePlaybackProgressAtom,
 } from '~/features/playback-progress-atom-context'
+import { codemirrorTheme } from '~/lib/codemirror/codemirror-theme'
+import { matchCodemirrorExtensions } from '~/lib/codemirror/match-codemirror-extensions'
+import { reversedChanges } from '~/lib/codemirror/reversed-changes'
+import { seekChanges } from '~/lib/codemirror/seek-changes'
+import { findClosestIndex } from '~/lib/collections/find-closest-index'
 import { readableDate } from '~/lib/date/readable-date'
 import { getCenterToScrollElemTo } from '~/lib/dom/get-center-to-scroll-elem-to'
 import { useConstAtom } from '~/lib/jotai/use-const-atom'
+import { mono } from '~/lib/media/fonts/mono'
 import { Image, type ImageProps } from '~/lib/media/image'
+import { matchFileTypeIcon } from '~/lib/media/match-file-type-icon'
 import { matchTagIcon } from '~/lib/media/match-tag-icon'
 import { BlurFade } from '~/lib/motion/blur-fade'
 import { motion } from '~/lib/motion/motion'
@@ -26,7 +46,11 @@ import { useConst } from '~/lib/react/use-const'
 import { useContainerHeightSync } from '~/lib/react/use-container-height-sync'
 import { useBackgroundEffect } from '~/lib/surfaces/background'
 import { shadowInsetStyles } from '~/lib/surfaces/shadow-inset-styles'
-import { usePostVmAtomValue } from './page.client'
+import type { Asset } from '~/model/asset'
+import type { AssetImageDynamic } from '~/model/assetImageDynamic'
+import type { AssetImageStatic } from '~/model/assetImageStatic'
+import type { AssetText } from '~/model/assetText'
+import { usePostVmAtom, usePostVmAtomValue } from './page.client'
 
 export function PostScrolling() {
   const captions = usePostVmAtomValue((it) => it.captions)
@@ -44,7 +68,7 @@ export function PostScrolling() {
 
   return (
     <PlaybackProgressAtomContext.Provider value={useConstAtom(0)}>
-      <article className="flex flex-col gap-16">
+      <article className={cx(mono.variable, 'flex flex-col gap-16')}>
         <BlurFade inView>
           <PostScrollingHeader />
         </BlurFade>
@@ -175,31 +199,299 @@ export function PostScrollingBody({ editor }: { editor: Editor }) {
 
   return (
     <div className="relative container" ref={containerRef}>
-      <div className="sticky top-0 h-dvh flex flex-col justify-center gap-1 border border-[white] border-dashed p-1 pr-8">
-        <motion.div
-          className="border border-[blue] overflow-y-hidden"
-          ref={contentRef}
-        >
+      <div className="sticky top-0 h-dvh flex flex-col justify-center gap-1 p-1 pr-8">
+        <motion.div className="overflow-y-hidden" ref={contentRef}>
           <EditorContent editor={editor} className="typography" />
         </motion.div>
-        <div
-          id="layout"
-          className="overflow-y-auto min-h-64 shrink-[9999] border border-[green]"
-        >
-          <div className="bg-accent-1 rounded-lg font-mono">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A cumque
-            harum, id, impedit in inventore itaque labore libero molestias
-            necessitatibus omnis provident quibusdam quis sint soluta tempore
-            vero, vitae voluptatibus. Lorem ipsum dolor sit amet, consectetur
-            adipisicing elit. Ab accusamus at commodi dignissimos, earum illo
-            incidunt magnam non, odit provident quaerat qui quia, repellat
-            reprehenderit sapiente suscipit temporibus unde? Quae? Lorem ipsum
-            dolor sit amet, consectetur adipisicing elit. Culpa harum ipsam iure
-            non obcaecati. Ad beatae blanditiis, consectetur consequatur ducimus
-            et laboriosam maxime minima, nemo optio rerum saepe sapiente ut?
-          </div>
-        </div>
+        <PostScrollingLayout />
       </div>
     </div>
   )
+}
+
+function PostScrollingLayout() {
+  const progressAtom = usePlaybackProgressAtom()
+
+  const changesAtom = usePostVmAtom((it) => it.layoutChanges)
+
+  const indexAtom = useConstAtom((get) =>
+    Option.fromNullable(
+      findClosestIndex(get(changesAtom), get(progressAtom), (it) => it.offset),
+    ),
+  )
+
+  const areasAtom = useConstAtom((get) =>
+    get(indexAtom).pipe(
+      Option.andThen((index) => Array.get(get(changesAtom), index)),
+      Option.andThen((it) => it.areas),
+    ),
+  )
+
+  const assetsAtom = usePostVmAtom((it) => it.assets)
+
+  const currentAssetsAtoms = useAtomValue(
+    useConst(() =>
+      splitAtom(
+        atom((get) =>
+          get(areasAtom).pipe(
+            Option.andThen((areas) =>
+              get(assetsAtom).filter((it) => areas.includes(it._id)),
+            ),
+            Option.getOrElse(() => []),
+          ),
+        ),
+      ),
+    ),
+  )
+
+  return (
+    <LayerGrid areasAtom={areasAtom}>
+      <AnimatePresence mode="popLayout">
+        {currentAssetsAtoms.map((assetAtom) => (
+          <MainLayerGridItem key={`${assetAtom}`} assetAtom={assetAtom} />
+        ))}
+      </AnimatePresence>
+    </LayerGrid>
+  )
+}
+
+function LayerGrid({
+  children,
+  areasAtom,
+}: PropsWithChildren<{ areasAtom: Atom<Option.Option<string>> }>) {
+  const areas = useAtomValue(
+    useConstAtom((get) => Option.getOrUndefined(get(areasAtom))),
+  )
+
+  return (
+    areas && (
+      <motion.ul
+        className="overflow-y-auto min-h-64 shrink-[9999] grid gap-2 *:h-full"
+        style={{
+          gridTemplateAreas: areas,
+          gridAutoColumns: 'minmax(0, 1fr)',
+          gridAutoRows: 'minmax(0, 1fr)',
+        }}
+      >
+        {children}
+      </motion.ul>
+    )
+  )
+}
+
+const JotaiMotionLi = jotai.create(motion.li)
+
+const matchLayoutAssetPanel = ({
+  _id,
+  type,
+}: Pick<typeof Asset.Type, '_id' | 'type'>) =>
+  Match.value(type).pipe(
+    Match.when('image-static', () => <LayoutAssetImageStatic _id={_id} />),
+    Match.when('image-dynamic', () => <LayoutAssetImageDynamic _id={_id} />),
+    Match.when('text', () => <LayoutAssetText _id={_id} />),
+    Match.exhaustive,
+  )
+
+const MainLayerGridItem = memo(
+  forwardRef<HTMLLIElement, { assetAtom: Atom<typeof Asset.Type> }>(
+    function MainLayerGridItem({ assetAtom }, ref) {
+      const idAtom = useConstAtom((get) => get(assetAtom)._id)
+
+      const styleAtom = useConstAtom((get) => ({ gridArea: get(idAtom) }))
+
+      const { _id, type } = useAtomValue(assetAtom)
+
+      return (
+        <JotaiMotionLi
+          ref={ref}
+          className="bg-bg/80 backdrop-blur-md border rounded-xl overflow-hidden aspect-video"
+          $style={styleAtom}
+          initial={{ opacity: 0, filter: 'blur(16px)' }}
+          animate={{ opacity: 1, filter: 'blur(0px)' }}
+          exit={{ opacity: 0, filter: 'blur(16px)' }}
+        >
+          {matchLayoutAssetPanel({ _id, type })}
+        </JotaiMotionLi>
+      )
+    },
+  ),
+)
+
+type LayoutAssetProps = Pick<typeof Asset.Type, '_id'>
+
+const useAsset = (_id: (typeof Asset.Type)['_id']) =>
+  useAtomValue(usePostVmAtom((it) => it.assets)).find((it) => it._id === _id)
+
+const LayoutAssetImageStatic = memo(function LayoutAssetImageStatic({
+  _id,
+}: LayoutAssetProps) {
+  const { name, href, alt, caption } = useAsset(_id) as AssetImageStatic
+
+  return (
+    <LayoutPanel>
+      <LayoutPanelHeader name={name} />
+      <Image
+        src={href}
+        className="object-cover blur-md"
+        alt="Image's backdrop blur"
+        fill
+      />
+      <section className="flex-1 relative">
+        <Image
+          src={href}
+          className="object-contain"
+          alt={alt.pipe(
+            Option.orElse(() => caption),
+            Option.getOrElse(
+              () => 'The author did not provide any alt to this image',
+            ),
+          )}
+          fill
+        />
+      </section>
+      {caption.pipe(
+        Option.andThen((c) => <LayoutPanelFooter>{c}</LayoutPanelFooter>),
+        Option.getOrNull,
+      )}
+    </LayoutPanel>
+  )
+})
+
+const LayoutAssetImageDynamic = memo(function LayoutAssetImageDynamic({
+  _id,
+}: LayoutAssetProps) {
+  const { name, href, caption } = useAsset(_id) as AssetImageDynamic
+
+  return (
+    <LayoutPanel>
+      <LayoutPanelHeader name={name} />
+      <video
+        className="absolute inset-0 size-full -z-[2] object-cover blur-lg"
+        src={href}
+        autoPlay
+        playsInline
+        muted
+        loop
+      />
+      <section className="flex-1 relative flex items-center">
+        <video
+          className="absolute inset-0 size-full object-contain object-center"
+          src={href}
+          autoPlay
+          playsInline
+          muted
+          loop
+        />
+      </section>
+      {caption.pipe(
+        Option.andThen((c) => <LayoutPanelFooter>{c}</LayoutPanelFooter>),
+        Option.getOrNull,
+      )}
+    </LayoutPanel>
+  )
+})
+
+const LayoutAssetText = memo(function LayoutAssetText({
+  _id,
+}: LayoutAssetProps) {
+  const { name, initialValue, advances } = useAsset(_id) as AssetText
+
+  const reverses = useMemo(
+    () => reversedChanges(initialValue, advances),
+    [initialValue, advances],
+  )
+
+  const cmRef = useRef<ReactCodeMirrorRef | null>(null)
+
+  const progressAtom = usePlaybackProgressAtom()
+
+  const headIndexAtom = useConstAtom((get) =>
+    findClosestIndex(advances, get(progressAtom), (it) => it[0]),
+  )
+
+  const anchorIndexAtom = useConstAtom<number | undefined>(undefined)
+
+  useAtomValue(
+    useConst(() =>
+      atomEffect((get, set) => {
+        const view = cmRef.current?.view
+        const state = view?.state
+
+        const anchor = get(anchorIndexAtom)
+        const head = get(headIndexAtom)
+
+        if (!(state && view)) {
+          return
+        }
+
+        const spec = seekChanges({
+          currentValue: state.doc,
+          initialValue,
+          advances,
+          reverses,
+          anchor,
+          head,
+        })
+
+        view.dispatch(spec)
+
+        set(anchorIndexAtom, head)
+      }),
+    ),
+  )
+
+  const extensions = useMemo(() => matchCodemirrorExtensions(name), [name])
+
+  const basicSetup: BasicSetupOptions = useConst(() => ({
+    lineNumbers: false,
+    foldGutter: false,
+    highlightActiveLine: false,
+  }))
+
+  return (
+    <LayoutPanel>
+      <LayoutPanelHeader name={name} />
+      <section className="flex-1 overflow-hidden">
+        <ReactCodeMirror
+          className={cx(
+            'h-full [&_.cm-editor]:h-full [&_.cm-scroller]:[scrollbar-width:thin] [&_.cm-scroller]:!text-xs md:[&_.cm-scroller]:!text-sm [&_.cm-line]:px-4',
+            '[&_.cm-scroller]:overflow-hidden',
+          )}
+          value={initialValue.toString()}
+          extensions={extensions}
+          editable={false}
+          theme={codemirrorTheme}
+          basicSetup={basicSetup}
+          ref={cmRef}
+        />
+      </section>
+    </LayoutPanel>
+  )
+})
+
+const panelEdgeClassName = cx(
+  'bg-overlay/75 py-2 px-3.5 text-sm flex items-center gap-1.5 z-10 tracking-wide',
+)
+
+function LayoutPanel({ children }: PropsWithChildren) {
+  return (
+    <article className="flex flex-col justify-between relative h-full">
+      {children}
+    </article>
+  )
+}
+
+function LayoutPanelHeader({ name }: { name: string }) {
+  const FileTypeIcon = matchFileTypeIcon(name)
+
+  return (
+    <header className={panelEdgeClassName}>
+      <FileTypeIcon className="size-4 text-muted-fg" />
+      {name.split('/').pop()}
+    </header>
+  )
+}
+
+function LayoutPanelFooter({ children }: PropsWithChildren) {
+  return <footer className={panelEdgeClassName}>{children}</footer>
 }
