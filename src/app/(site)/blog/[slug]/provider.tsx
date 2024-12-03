c27e64c1eb66be6be8ba8958a8beecd56b931400
Boaris <boriszubc@gmail.com>
2024-12-03 16:41:10 +0200

    bc

--- src/app/(site)/blog/[slug]/provider.tsx
+++ src/app/(site)/blog/[slug]/provider.tsx
@@ -5,14 +5,14 @@ import { Array, Option, Schema, pipe } from 'effect'
 import type { NonEmptyReadonlyArray } from 'effect/Array'
 import { atom, useStore } from 'jotai'
 import { useSetAtom } from 'jotai/index'
-import { type FC, type PropsWithChildren, useEffect } from 'react'
+import type { FC, PropsWithChildren } from 'react'
 import { readableDate } from '~/lib/date/readable-date'
 import { getCenterToScrollElemTo } from '~/lib/dom/get-center-to-scroll-elem-to'
-import { createAtomContext } from '~/lib/jotai/create-atom-context'
 import type { ImageIconProps } from '~/lib/media/icons/_base'
 import { matchTagIcon } from '~/lib/media/match-tag-icon'
 import { findBlockAncestorDepth } from '~/lib/pm/find-block-ancestor-depth'
 import type { JSONContent } from '~/lib/pm/json-content'
+import { createStrictContext } from '~/lib/react/create-strict-context'
 import { Post } from '~/model/post'
 
 export type PostPageContextValue = {
@@ -36,7 +36,7 @@ export type PostPageContextValue = {
 }
 
 export const [PostPageContext, usePostPage] =
-  createAtomContext<PostPageContextValue>({
+  createStrictContext<PostPageContextValue>({
     name: 'PostPageContext',
   })
 
@@ -76,59 +76,53 @@ export function PostPageProvider({
 
   const store = useStore()
 
-  const value: PostPageContextValue = {
-    title,
-    lead,
-    posterUrl,
-    date,
-    tags,
-    captions,
-    setDocSize: useSetAtom(docSizeAtom),
-    setProgress: useSetAtom(progressAtom),
-    scrollableEffect: ({
-      element,
-      dispatchPosition,
-      resolvePosition,
-      nodeDom,
-    }) => {
-      return store.sub(positionAtom, () => {
-        const position = store.get(positionAtom)
-
-        dispatchPosition(position)
-
-        if (position === 0) {
-          return element.scrollTo({ top: 0, behavior: 'smooth' })
-        }
-
-        const $pos = resolvePosition(position)
-
-        const depth = findBlockAncestorDepth($pos)
-        if (depth === undefined) {
-          return
-        }
-
-        const blockElement = nodeDom($pos.before(depth))
-        if (!(blockElement instanceof Element)) {
-          return
-        }
-
-        element.scrollTo({
-          top: getCenterToScrollElemTo(element, blockElement),
-          behavior: 'smooth',
-        })
-      })
-    },
-  }
-
-  const v = atom(value)
-
-  useEffect(() => {
-    store.sub(v, () => {
-      console.log(v)
-    })
-  }, [store, v])
-
   return (
-    <PostPageContext.Provider value={v}>{children}</PostPageContext.Provider>
+    <PostPageContext.Provider
+      value={{
+        title,
+        lead,
+        posterUrl,
+        date,
+        tags,
+        captions,
+        setDocSize: useSetAtom(docSizeAtom),
+        setProgress: useSetAtom(progressAtom),
+        scrollableEffect: ({
+          element,
+          dispatchPosition,
+          resolvePosition,
+          nodeDom,
+        }) => {
+          return store.sub(positionAtom, () => {
+            const position = store.get(positionAtom)
+
+            dispatchPosition(position)
+
+            if (position === 0) {
+              return element.scrollTo({ top: 0, behavior: 'smooth' })
+            }
+
+            const $pos = resolvePosition(position)
+
+            const depth = findBlockAncestorDepth($pos)
+            if (depth === undefined) {
+              return
+            }
+
+            const blockElement = nodeDom($pos.before(depth))
+            if (!(blockElement instanceof Element)) {
+              return
+            }
+
+            element.scrollTo({
+              top: getCenterToScrollElemTo(element, blockElement),
+              behavior: 'smooth',
+            })
+          })
+        },
+      }}
+    >
+      {children}
+    </PostPageContext.Provider>
   )
 }
