42a1427e2fd85416ccca326185e6f8c148611686
Boris Zubchenko <boriszubc@gmail.com>
2024-12-07 18:14:48 +0200

    bc

--- src/lib/dom/get-center-to-scroll-elem-to.ts
+++ src/lib/dom/get-center-to-scroll-elem-to.ts
@@ -1,21 +1,23 @@
 export const getCenterToScrollElemTo = (
-  scrollable: Element,
-  element: Element,
+  scrollableElement: Element,
+  targetElement: Element,
 ) => {
-  const scrollableRect = scrollable.getBoundingClientRect()
-  const elementRect = element.getBoundingClientRect()
+  const scrollableRect = scrollableElement.getBoundingClientRect()
+  const targetRect = targetElement.getBoundingClientRect()
 
-  // Distance from the top of the scrollable container to the element
-  const elementOffsetTop = elementRect.top - scrollableRect.top
+  // Distance from the top of the scrollable container to the target element
+  const targetRelativeTop = targetRect.top - scrollableRect.top
+
+  // Midpoint of the scrollable container and target element
+  const scrollableMidpoint = scrollableElement.clientHeight / 2
+  const targetMidpoint = targetRect.height / 2
 
   // Calculate the center position to scroll to
   const centerScrollTop =
-    scrollable.scrollTop +
-    elementOffsetTop -
-    scrollable.clientHeight / 2 +
-    elementRect.height / 2
-
-  console.log(elementRect)
+    scrollableElement.scrollTop +
+    targetRelativeTop -
+    scrollableMidpoint +
+    targetMidpoint
 
   return centerScrollTop
 }
