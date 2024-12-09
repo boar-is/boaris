07318392120a526f07b1e36b52f2522a5019006e
Boaris <boriszubc@gmail.com>
2024-12-09 21:00:38 +0200

    bc

--- src/features/playback-progress-atom-context.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import type { PrimitiveAtom } from 'jotai'
-import { createStrictContext } from '~/lib/react/create-strict-context'
-
-export const [PlaybackProgressAtomContext, usePlaybackProgressAtom] =
-  createStrictContext<PrimitiveAtom<number>>({
-    name: 'PlaybackProgressAtomContext',
-  })
