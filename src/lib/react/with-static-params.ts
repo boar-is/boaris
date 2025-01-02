9670c8e7a2e919231bc69acba2c26cd16e1bd946
Boaris <boriszubc@gmail.com>
2025-01-02 06:02:45 +0200

    wip

--- src/lib/react/with-static-params.ts
+++ src/lib/react/with-static-params.ts
@@ -1,4 +1,5 @@
-export type WithStaticParams<F extends () => Promise<ReadonlyArray<unknown>>> =
-  F extends () => Promise<ReadonlyArray<infer T>>
-    ? { params: Promise<T> }
-    : never
+export type WithStaticParams<F> = F extends () => Promise<
+  ReadonlyArray<infer T>
+>
+  ? { params: Promise<T> }
+  : never
