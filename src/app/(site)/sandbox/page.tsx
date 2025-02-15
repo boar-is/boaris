c2eab20e12bdf70742d5d174b9ac6664dd9ccb8a
Boris <boriszubc@gmail.com>
2025-02-15 23:27:55 +0200

    wip

--- src/app/(site)/sandbox/page.tsx
+++ src/app/(site)/sandbox/page.tsx
@@ -1,8 +1,13 @@
+import { redirect } from 'next/navigation'
 import { mono } from '~/lib/media/fonts/mono'
 import { cx } from '~/lib/react/cx'
 import { SandboxText } from './_text'
 
 export default function SandboxPage() {
+  if (globalThis.process.env.NODE_ENV === 'production') {
+    return redirect('/')
+  }
+
   return (
     <div className={cx(mono.variable)}>
       <SandboxText />
