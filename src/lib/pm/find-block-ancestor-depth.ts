ebf4bb17234231657ee7193542368342ea8cce7f
Boris <boriszubc@gmail.com>
2025-02-18 00:40:02 +0200

    wip

--- src/lib/pm/find-block-ancestor-depth.ts
+++ src/lib/pm/find-block-ancestor-depth.ts
@@ -1,11 +1,18 @@
 import type { ResolvedPos } from '@tiptap/pm/model'
 
+const topNodeBannedTypes = new Set([
+  'doc',
+  'bulletList',
+  'orderedList',
+  'listItem',
+])
+
 export const findBlockAncestorDepth = ($pos: ResolvedPos) => {
   let depth = $pos.depth
 
   while (depth > 0) {
     const node = $pos.node(depth)
-    if (node.isBlock) {
+    if (node.isBlock && node.inlineContent) {
       return depth
     }
     depth--
@@ -13,7 +20,7 @@ export const findBlockAncestorDepth = ($pos: ResolvedPos) => {
 
   // If the top-level node is a block node (excluding the document node)
   const topNode = $pos.node(0)
-  if (topNode.isBlock && topNode.type.name !== 'doc') {
+  if (topNode.isBlock && !topNodeBannedTypes.has(topNode.type.name)) {
     return 0
   }
 
