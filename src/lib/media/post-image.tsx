a8d707339f353efdb0163a919be5bd7555a68d64
Boris <boriszubc@gmail.com>
2025-03-03 14:12:03 +0200

    wip

--- src/lib/media/post-image.tsx
+++ src/lib/media/post-image.tsx
@@ -1,5 +1,17 @@
+import { cx } from '~/lib/react/cx'
+import { shadowInsetStyles } from '~/lib/surfaces/shadow-inset-styles'
 import { Image, type ImageProps, defaultImageSizes } from './image'
 
 export function PostImage(props: Pick<ImageProps, 'src' | 'alt'>) {
-  return <Image {...props} sizes={defaultImageSizes} placeholder="blur" />
+  return (
+    <Image
+      {...props}
+      sizes={defaultImageSizes}
+      placeholder="blur"
+      className={cx(
+        shadowInsetStyles,
+        '~rounded-xl/2xl after:~rounded-xl/2xl bg-clip-padding border border-white/10',
+      )}
+    />
+  )
 }
